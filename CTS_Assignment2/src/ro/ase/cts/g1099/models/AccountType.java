ce57322d5ebed7b2a9735158d2c394570c7dc650
Alice <aliceradu@ymail.com>
2022-04-05 14:31:33 +0300

    Stage 1

---
+++ CTS_Assignment2/src/ro/ase/cts/g1099/models/AccountType.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.g1099.models;
+
+public enum AccountType {
+	STANDARD(0),
+	BUDGET(1),
+	PREMIUM(2),
+	SUPER_PREMIUM(3);
+	
+	int type;
+	
+	private AccountType(int type) {
+		this.type=type;
+	}
+	
+	public int getType() {
+		return type;
+	}	
+}
